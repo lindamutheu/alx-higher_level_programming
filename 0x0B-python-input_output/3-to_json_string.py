99308104ec36b2b06c32ea3681176ebe1f896ee8
lindamutheu <lindamutheu5@gmail.com>
2022-08-02 09:57:42 -0700

    commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,11 @@
-#!/bin/bash
+#!/usr/bin/python3
+"""
+Contains the "to_json_string" fundtion
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
